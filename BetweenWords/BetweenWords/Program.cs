fc5a08e0cd4334c87b7163fd1ea3c0ac25ada511
djsb91 <sbaranczuk@gmail.com>
2020-03-25 20:23:48 +0100

    Between Words - Done

--- BetweenWords/BetweenWords/Program.cs
+++ BetweenWords/BetweenWords/Program.cs
@@ -4,9 +4,16 @@ namespace BetweenWords
 {
     class Program
     {
+        public static bool isBetween(string first, string last, string word)
+        {
+            return first.CompareTo(word) == -1 && last.CompareTo(word) == 1;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(isBetween("apple", "banana", "azure"));
+            Console.ReadKey();
         }
     }
 }
