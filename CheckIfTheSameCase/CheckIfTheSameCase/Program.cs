c7f15d1bb85b59cfded010bfb492deae2c787332
djsb91 <sbaranczuk@gmail.com>
2020-03-25 20:39:12 +0100

    Check if the same case - Done

---
+++ CheckIfTheSameCase/CheckIfTheSameCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CheckIfTheSameCase
+{
+    class Program
+    {
+        public class IsLowerSample
+        {
+            public static bool SameCase(string str)
+            {
+                return str == str.ToUpper() | str == str.ToLower();
+            }
+
+            public static void Main()
+            {
+                Console.WriteLine(SameCase("hello"));
+                Console.WriteLine(SameCase("HELLO"));
+                Console.WriteLine(SameCase("Hello"));
+                Console.WriteLine(SameCase("ketcHUp"));
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
